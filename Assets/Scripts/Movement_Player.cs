66fff9662974928fe3c9d9639e5660affbbc34b2
Qa160601 <luonyeuvo2001@gmail.com>
2021-11-08 01:41:18 +0700

    Movement Player

--- Assets/Scripts/Movement_Player.cs
+++ Assets/Scripts/Movement_Player.cs
@@ -4,15 +4,58 @@ using UnityEngine;
 
 public class Movement_Player : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
-    {
-        
-    }
+    #region Public Values
+    public float moveSpeed;
+    public Rigidbody2D rb;
+    public Animator animator;
+    public SpriteRenderer SpriteRenderer;
+
+    #endregion
+
+    #region Private Values
+    private Vector2 movement;
+    private bool TurnLeft;
+
+    #endregion
 
     // Update is called once per frame
     void Update()
     {
-        
+        // Input
+        movement.x = Input.GetAxisRaw("Horizontal");
+        movement.y = Input.GetAxisRaw("Vertical");
+
+
+        animator.SetFloat("Horizontal", movement.x);
+        animator.SetFloat("Vertical", movement.y);
+        animator.SetFloat("Speed", movement.sqrMagnitude);
+
+    }
+
+    private void FixedUpdate()
+    {
+        // Movement
+        rb.MovePosition(rb.position + movement * moveSpeed * Time.fixedDeltaTime);
+    }
+
+    private void TurnPlayer()
+    {
+        if (animator.GetFloat("Horizontal") < 0)
+        {
+            TurnLeft = true;
+        }
+        else if (animator.GetFloat("Horizontal") > 0)
+        {
+            TurnLeft = false;
+        }
+
+        if (TurnLeft)
+        {
+            SpriteRenderer.flipX = true;
+        }
+        else
+        {
+            SpriteRenderer.flipX = false;
+        }
     }
 }
