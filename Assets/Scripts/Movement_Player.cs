5c3d8d3b0dad3a807368f9bf8f1bf3633e224bdc
Qa160601 <luonyeuvo2001@gmail.com>
2021-11-09 01:03:32 +0700

    test

--- Assets/Scripts/Movement_Player.cs
+++ Assets/Scripts/Movement_Player.cs
@@ -30,7 +30,7 @@ public class Movement_Player : MonoBehaviour
         animator.SetFloat("Horizontal", movement.x);
         animator.SetFloat("Vertical", movement.y);
         animator.SetFloat("Speed", movement.sqrMagnitude);
-
+        
     }
 
     private void FixedUpdate()
